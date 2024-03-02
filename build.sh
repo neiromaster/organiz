65ea216e081c58e9e3ec17b386109850560f2e03
neiromaster <parrysh@mail.ru>
2024-03-02 14:23:15 +0300

    new build script

---
+++ build.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+# A function for processing the source directive
+process_include() {
+    local file="$1"
+
+    while IFS= read -r line; do
+        if [[ "$line" =~ ^\source\ (.+) ]]; then
+            process_include "${BASH_REMATCH[1]}"
+        else
+            echo "$line"
+        fi
+    done <"$file"
+}
+
+# Starting file processing
+process_include organiz.sh | sed -e '2,$ s/^#!.*//' -e '/^$/ d' > build/organiz.sh
+
