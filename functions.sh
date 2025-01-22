f7726b9dc1b7a93bf7f258042f937f3f20b33e51
Maxim (neiromaster) Gavrilenko <parrysh@mail.ru>
2025-01-22 17:33:54 +0500

    refactor: Remove unnecessary log message in update_script function

--- functions.sh
+++ functions.sh
@@ -423,8 +423,6 @@ function update_script() {
   local release_number
   release_number=$(echo "$script_url" | awk -F'/' '{print $(NF-1)}')
 
-  log_message "Release number: $release_number"
-
   # Check if the download was successful
   if [ "$release_number" = "###########" ]; then
     log_message "The script is already up-to-date."
